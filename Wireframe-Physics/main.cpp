9f17b14d45f7ff15f135a9fe89f74c6ea7cf302c
Ahmed Samy <marcheder200.200@gmail.com>
2025-07-06 17:04:41 +0300

    oneTBB Threading Test

--- Wireframe-Physics/main.cpp
+++ Wireframe-Physics/main.cpp
@@ -1,22 +1,42 @@
 #include <iostream>
 #include <sstream>
 #include <mutex>
+#include <chrono>
 #include "World.h"
-
+// MultiThreading 7856.94 ms 
+// Normal         58722.1 ms
 int main() {
-	World world;
-	auto obj = world.AddRigidBody(BodyType::Dynamic, 5.0f, { 0.0f,1.0f,0.0f });
-	auto obj1 = world.AddRigidBody(BodyType::Dynamic, 4.0f, { 8.0f,1.0f,0.0f });
-	std::vector<std::string> BodiesLogLines(world.bodies.size());
-	tbb::parallel_for(size_t(0), world.bodies.size(), size_t(1), [&](size_t i)
-	{
-			std::ostringstream ss;
-			ss << "Object With ID : " << i <<
-			", Mass : " << world.bodies[i]->mass <<
-			", Position : (x)" << world.bodies[i]->position.x() 
-			<< ", (y)" << world.bodies[i]->position.y() << 
-			", (z)" << world.bodies[i]->position.z();
-			BodiesLogLines[i] = ss.str();
-	});
-	for (const auto& line : BodiesLogLines) { std::cout << line << '\n'; }
+    auto start = std::chrono::high_resolution_clock::now();
+    World world;
+    std::mutex add_mutex;
+
+    tbb::parallel_for(tbb::blocked_range<size_t>(0, 100001), [&](const tbb::blocked_range<size_t>& range)
+        {
+            for (size_t i = range.begin(); i != range.end(); ++i) {
+                float x = static_cast<float>(i % 100);
+                float y = 1.0f;
+                float z = static_cast<float>(i / 100);
+                float mass = 1.0f + (i % 10);
+
+                std::lock_guard<std::mutex> lock(add_mutex);
+                world.AddRigidBody(BodyType::Dynamic, mass, { x, y, z });
+            }
+        }
+    );
+    std::vector<std::string> BodiesLogLines(world.bodies.size());
+    tbb::parallel_for(size_t(0), world.bodies.size(), size_t(1), [&](size_t i)
+    {
+            std::ostringstream ss;
+            ss << "Object With ID : " << i <<
+            ", Mass : " << world.bodies[i]->mass <<
+            ", Position : (x)" << world.bodies[i]->position.x() 
+            << ", (y)" << world.bodies[i]->position.y() << 
+            ", (z)" << world.bodies[i]->position.z();
+            BodiesLogLines[i] = ss.str();
+    });
+    for (const auto& line : BodiesLogLines) { std::cout << line << '\n'; }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> duration = end - start;
+    std::cout << "\nExecution Time: " << duration.count() << " ms\n ";
 }
